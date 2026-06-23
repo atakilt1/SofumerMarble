46dd7efe10d75d6c02efbde26aee2c6fb3e004e1
atakilt1 <atakilt.send@gmail.com>
2026-06-23 14:26:28 +0300

    get all categories and display

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,23 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using SofumerMarble.Data;
+using SofumerMarble.Models;
 
 namespace SofumerMarble.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objList  = _db.Category.ToList();
+            return View(objList);
         }
     }
 }
