3de1b27ba49e3d1c9831fd1a86ef12c6c97551fb
atakilt1 <atakilt.send@gmail.com>
2026-06-23 22:46:48 +0300

    added update for category & application type

--- Controllers/ApplicationTypeController.cs
+++ Controllers/ApplicationTypeController.cs
@@ -22,8 +22,7 @@ namespace SofumerMarble.Controllers
 
         //GET - CREATE
         public IActionResult Create()
-        {
-            
+        {            
             return View();
         }
 
@@ -38,6 +37,36 @@ namespace SofumerMarble.Controllers
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
+            return View(obj);
+        }
+
+        //GET - Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ApplicationType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ApplicationType.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
             return View(obj);
         }
     }
--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -42,5 +42,35 @@ namespace SofumerMarble.Controllers
 
             return View(category);
         }
+
+        //GET - Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
     }
 }
