7b2c319b9d0c03eacbae1b227fca6880df81d417
atakilt1 <atakilt.send@gmail.com>
2026-06-25 22:59:52 +0300

    Added Product model

---
+++ Models/Product.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SofumerMarble.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Product name is required")]
+         [StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Range(0.1, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Url(ErrorMessage = "Invalid image URL")]
+        public string Image { get; set; }
+
+        [Display(Name = "Category Type")]
+        
+        [Required]
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+    }
+}
